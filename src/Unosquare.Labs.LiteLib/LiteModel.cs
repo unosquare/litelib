10a11daef59922d0e6c34767a0f5208f4058452c
Geo Perez <geovanni.perez@gmail.com>
2018-07-02 09:22:27 -0500

    Missing file

---
+++ src/Unosquare.Labs.LiteLib/LiteModel.cs
@@ -0,0 +1,12 @@
+﻿namespace Unosquare.Labs.LiteLib
+{
+    /// <summary>
+    /// Base model class for ISQLiteEntity.
+    /// Inherit from this model if you don't want to implement the RowId property.
+    /// </summary>
+    public abstract class LiteModel : ILiteModel
+    {
+        /// <inheritdoc />
+        public long RowId { get; set; }
+    }
+}
